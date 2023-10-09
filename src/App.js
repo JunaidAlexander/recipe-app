b677dbbbb51d301bc3f173bc0aa96635c5637426
TshegoLetlape <136719938+TshegoLetlape@users.noreply.github.com>
2023-10-09 17:32:17 +0200

    card styling

--- src/App.js
+++ src/App.js
@@ -5,52 +5,57 @@ import "./App.css";
 import "./Card.css";
 
 const App = () => {
-	return (
-		<div>
-			<Header />
+  return (
+    <div>
+      <Header />
 
-			<div>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/creamy-tomato-pasta.jpg"
-					// button="Click for Recipe"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/pasta-bake-with-sausage.jpg"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/pesto-penne-pasta.jpg"
-				/>
+      <div>
+        <Card
+          titleFront="Baked Gnocchi"
+          recipeText="Click For Recipe"
+          imageUrl="./images/baked-gnocchi.jpg
+          "
+          // button="Recipe"
+        />
+        <Card
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/pasta-bake-with-sausage.jpg"
+        />
+        <Card
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/pesto-penne-pasta.jpg"
+        />
 
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/fettuccine-alfredo.jpg"
-				/>
         <Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/cheesy-tortellini.jpg"
-				/>
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/fettuccine-alfredo.jpg"
+        />
         <Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/shrimp-alfredo.jpg"
-				/>
-			</div>
-		</div>
-	);
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/cheesy-tortellini.jpg"
+        />
+        <Card
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/shrimp-alfredo.jpg"
+        />
+        <Card
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/spaghetti-and-meatballs.jpg"
+        />
+        <Card
+          titleFront="PASTA BAKE WITH SAUSAGE"
+          recipeText="Click For Recipe"
+          imageUrl="./images/vegetarian-bolognese.jpg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,25 @@
 import React from "react";
 import "./Card.css";
 
-const Card = props => {
-	const { titleFront, titleBack, recipeText, imageUrl } = props;
-	return (
-		<div className="card">
-			<div className="myCard">
-				<div className="innerCard">
-					<div className="frontSide">
-						<img src={imageUrl} alt={titleFront} className="card-image" />
-						<p className="title-front">
-							{titleFront}
-						</p>
-						<p className="title-back">
-							{titleBack}
-						</p>
-					</div>
-					<div className="backSide">
-						<p className="title">
-							{recipeText}
-						</p>
-						<button>Click  for recipe</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+const Card = (props) => {
+  const { titleFront, titleBack, recipeText, imageUrl } = props;
+  return (
+    <div className="card">
+      <div className="myCard">
+        <div className="innerCard">
+          <div className="frontSide">
+            <img src={imageUrl} alt={titleFront} className="card-image" />
+            <p className="title-front">{titleFront}</p>
+            <p className="title-back">{titleBack}</p>
+          </div>
+          <div className="backSide">
+            <p className="title">{recipeText}</p>
+            <button className="btn">Recipe</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
