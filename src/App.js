f51859e5b0dce388beb25cd1110cbfe2fbdad0ed
TshegoLetlape <136719938+TshegoLetlape@users.noreply.github.com>
2023-10-11 12:56:29 +0200

    app

--- src/App.js
+++ src/App.js
@@ -5,51 +5,56 @@ import "./App.css";
 import "./Card.css";
 
 const App = () => {
-	return (
-		<div>
-			<Header />
-			<div>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/creamy-tomato-pasta.jpg"
-					// button="Click for Recipe"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/pasta-bake-with-sausage.jpg"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/pesto-penne-pasta.jpg"
-				/>
+  return (
+    <div>
+      <Header />
 
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/fettuccine-alfredo.jpg"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/cheesy-tortellini.jpg"
-				/>
-				<Card
-					titleFront="PASTA BAKE WITH SAUSAGE"
-					titleBack="Hover Me"
-					recipeText="Click For Recipe"
-					imageUrl="./images/shrimp-alfredo.jpg"
-				/>
-			</div>
-		</div>
-	);
+      <div>
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/baked-gnocchi.jpg"
+          dishName="BAKED GNOCCHI"
+          // button="Recipe"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="https://www.aheadofthyme.com/wp-content/uploads/2022/01/beef-stroganoff-683x1024.jpg"
+          dishName="PASTA & SAUSAGE"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/pesto-penne-pasta.jpg"
+          dishName="PESTO PENNE PASTA"
+        />
+
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/fettuccine-alfredo.jpg"
+          dishName="FETTUCCINE ALFREDO"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/cheesy-tortellini-.jpg"
+          dishName="CHEESY TORTELLINI"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/shrimp-alfredo.jpg"
+          dishName="SHRIMP ALFREDO"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/spaghetti-and-meatballs.jpg"
+          dishName="SPAGHETTI MEATBALLS"
+        />
+        <Card
+          recipeText="Click For Recipe"
+          imageUrl="./images/vegetarian-bolognese.jpg"
+          dishName="VEGETARIAN BOLOGNESE"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
